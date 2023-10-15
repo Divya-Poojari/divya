01d00282fa7a9d5b5ccc8891eebf2f8b32f4ff4f
Divya <divyapoojari125@gmail.com>
2023-10-15 12:44:09 +0530

    third commit

---
+++ D5-index--CW.js
@@ -0,0 +1,114 @@
+//alert("Hello DIVYA");
+
+//console.log("Hello Divya");
+//console.log("3+9");
+
+//document.write("<h1>Javascript is programming language</h1>")
+//variables
+//var a=5;
+//var a=6;//redeclare the value
+//a=7//reassign the value
+//console.log(a);
+
+//let b=25;
+//let b=26; //we cant redeclare the variable
+//b=26; //we can reassign the value
+//console.log(b);
+
+//const c=66;
+//const c=33; //we cant redeclare
+//c=90; //we cannot rteassign the value
+//console.log(c);
+
+
+
+//let a123="sdbcnk"
+
+//let a;
+//let b=null; //Bug in JS-datatype of null is always object
+//console.log(typeof(a)); //typeof operator used to check datatype of variable
+//console.log(typeof(b));//
+
+//console.log(3**6);
+//let num1=25
+//num1++;
+//console.log(num1)
+//num1--;
+//console.log(num1)
+
+//let x=5
+//x+=6
+//console.log(x)
+
+//let x=5
+//x-=6
+//console.log(x)
+
+//let x=5
+//x**=6
+//console.log(x)
+
+//let num1 = 55;
+//let num2 = 66;
+//console.log(num1==num2);
+
+//let num1a = 55;
+//let num2a = "55";//ans is true bcz == checks only value but not datatype
+//console.log(num1==num2);
+
+//let num1ab = 55;
+//let num2ab = "55";//ans is false bcz === checks both value and datatype ( "" is string)
+//console.log(num1===num2);
+
+//function add(num1 , num2 , num3){
+  //  return num1+num2+num3;
+//}
+//document.write(add(15,7,8))
+
+//function greetings(){
+  //  alert("Hellooooooooo");
+//}
+
+
+
+//let age=4;
+//if(age>=18){
+   // alert('Eligible to vote');
+//    document.write("Eligible for vote")
+//}
+//else{
+   // alert("Not eligible")
+  // document.write("Not eligible")
+//}
+
+//const person={
+//name:"Anush",
+//age:20,
+//gender:"Male",
+//email:"123@gmail.com";
+//mob:9876543210;
+//};
+//console.log(person);
+//console.log(person.name);
+//console.log(person.age);
+//console.log(person.mob);
+//console.log(person.email);
+//document.write(person.name);
+
+//const cars=["A","B","C","D"];
+//console.log(cars[3]) //C is output
+//console.log(cars.length);
+
+//for(let num1=1;num1<=10;num1++){
+    //document.write(num1);
+  //  console.log(num1);
+//}
+
+//DOM Manipulation
+//let el=document.getElementsById("header").innerHTML;
+//el.innerHTML="Javascript";
+//el.value;
+//console.log(el);
+
+//let el2=document.querySelector("#header").innerHTML;
+//console.log(el2);
